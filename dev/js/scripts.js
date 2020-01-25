1db41e48e71013ecde2f8d02eb081c94ff13b05d
Uskov Ruslan <uskov.submail@gmail.com>
2020-01-25 18:32:42 +0300

    Desktop version of the home page

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,4 +20,39 @@ $(function () {
       }
     ]
   });
+
+  // Карусель новостей
+  $('#news-names').slick({
+    fade: true,
+    adaptiveHeight: true,
+    appendArrows: '#news-arrows',
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/icons-sprite.svg#icon-angle"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/icons-sprite.svg#icon-angle"></use></svg></button>',
+    asNavFor: '#news-carousel',
+    swipe: false,
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          
+        }
+      }
+    ]
+  });
+
+  $('#news-carousel').slick({
+    variableWidth: true,
+    arrows: false,
+    dots: false,
+    asNavFor: '#news-names',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          variableWidth: false
+        }
+      }
+    ]
+  });
 });
